1c8f44afae0ed9fa253439323095d1acf0763d44
liangxchc <liangxchc@yonyou.com>
2020-07-17 09:38:47 +0800

    初始化用友通信运维项目

---
+++ src/main/java/com/yytx/maintenance/YytxMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.yytx.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YytxMaintenanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YytxMaintenanceApplication.class, args);
+    }
+
+}
