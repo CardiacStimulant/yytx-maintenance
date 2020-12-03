c62cc296e953f5f5a639e0401a3f332c38b823b8
liangxchc <liangxchc@yonyou.com>
2020-12-03 19:15:17 +0800

    删除Scope注解

--- src/main/java/com/maintenance/base/service/UserManagerService.java
+++ src/main/java/com/maintenance/base/service/UserManagerService.java
@@ -22,8 +22,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +29,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-@Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class UserManagerService {
     private Logger logger = LoggerFactory.getLogger(UserManagerService.class);
 
